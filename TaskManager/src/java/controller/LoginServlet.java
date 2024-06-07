04a3177f864e84d080c3dd35a3694beceadcfdf1
namdng09 <namduong090104@gmail.com>
2024-06-07 23:00:25 +0700

    feat: change code in doGet func

--- TaskManager/src/java/controller/LoginServlet.java
+++ TaskManager/src/java/controller/LoginServlet.java
@@ -56,7 +56,7 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        processRequest(request, response);
+        request.getRequestDispatcher("login.jsp").forward(request, response);
     } 
 
     /** 
