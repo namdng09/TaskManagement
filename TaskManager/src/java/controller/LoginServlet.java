84a64b374f72382c841ca74f4352f6dd312ac8c7
namdng09 <144111926+namdng09@users.noreply.github.com>
2024-06-07 23:18:03 +0700

    refactor: delete space line

--- TaskManager/src/java/controller/LoginServlet.java
+++ TaskManager/src/java/controller/LoginServlet.java
@@ -56,7 +56,6 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        
         request.getRequestDispatcher("login.jsp").forward(request, response);
     } 
 
