c409be52f6bd21c4289145ad2ec8eb72b898647b
namdng09 <144111926+namdng09@users.noreply.github.com>
2024-06-07 23:13:14 +0700

    test

--- TaskManager/src/java/controller/LoginServlet.java
+++ TaskManager/src/java/controller/LoginServlet.java
@@ -56,6 +56,7 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
+        
         request.getRequestDispatcher("login.jsp").forward(request, response);
     } 
 
