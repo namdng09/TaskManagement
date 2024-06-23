5a0ae95db05a196a9df47b02e85b60147b7d3402
namdng09 <144111926+namdng09@users.noreply.github.com>
2024-06-24 01:00:47 +0700

    feat: dao files to access to table in database (#29)

---
+++ TaskManager/src/java/dao/CardDAO.java
@@ -0,0 +1,34 @@
+package dao;
+
+import java.sql.*;
+import model.User;
+
+public class CardDAO {
+    
+    /**
+     * Create instance for PreparedStatement class
+     *
+     * @param query the SQL query to be prepared.
+     *
+     * @return a PreparedStatement object that can be used to execute the query
+     * with specified parameters.
+     *
+     * @throws SQLException if a database access error occurs or the SQL
+     * statement cannot be pre-compiled.
+     */
+    public PreparedStatement createPreparedStatement(String query) throws SQLException {
+        Connection conn = DBContext.getInstance().getConnection();
+        return conn.prepareStatement(query);
+    }
+
+    /**
+     * Executes a SQL query using the provided PreparedStatement.
+     *
+     * @param pstmt the PreparedStatement to be executed.
+     * @return the ResultSet obtained from executing the query.
+     * @throws java.sql.SQLException
+     */
+    public ResultSet executeQuery(PreparedStatement pstmt) throws SQLException {
+        return pstmt.executeQuery();
+    }
+}
