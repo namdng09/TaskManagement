1600d1118f08f8d7d966cf4ca73a89840ce0a17a
namdng09 <144111926+namdng09@users.noreply.github.com>
2024-06-27 08:32:45 +0700

    feat: dao file to get value in Broad table in database(#29)

--- TaskManager/src/java/dao/BroadDAO.java
+++ TaskManager/src/java/dao/BroadDAO.java
@@ -5,6 +5,75 @@ import model.Broad;
 
 public class BroadDAO {
 
+    public void insertBroad(Broad broad, String useruid) throws SQLException {
+        String query = "INSERT INTO [dbo].[Broad]"
+                + "([BroadID],[User_UID],[Name],[CreatedDate],[isPublic],[image])"
+                + "VALUES (?,?,?,?,?,?)";
+        PreparedStatement pstmt = createPreparedStatement(query);
+        pstmt.setString(1, broad.getBroadID());
+        pstmt.setString(2, useruid);
+        pstmt.setString(3, broad.getBroadName());
+        pstmt.setDate(4, broad.getCreateDate());
+        pstmt.setBoolean(5, broad.isIsPublic());
+        pstmt.setString(6, broad.getImage());
+
+        pstmt.executeUpdate();
+    }
+
+    public void deleteBroad(String broadID) throws SQLException {
+        String query = "DELETE FROM [dbo].[Broad] WHERE [BroadID] = ?";
+        PreparedStatement pstmt = this.createPreparedStatement(query);
+        pstmt.setString(1, broadID);
+
+        pstmt.executeUpdate();
+    }
+
+    public void renameBroad(String broadID, String newName) throws SQLException {
+        String query = "UPDATE [dbo].[Broad] SET [Name] = ? WHERE [BroadID] = ?";
+        PreparedStatement pstmt = this.createPreparedStatement(query);
+        pstmt.setString(1, newName);
+        pstmt.setString(2, broadID);
+
+        pstmt.executeUpdate();
+    }
+
+    public ResultSet getAllBroadByUserUID(String useruid) throws SQLException {
+        String query = "SELECT * FROM [dbo].[Broad] WHERE [USER_ID] = ?";
+        PreparedStatement pstmt = createPreparedStatement(query);
+        // modify the query here
+        pstmt.setString(1, useruid);
+        return executeQuery(pstmt);
+    }
+
+    public ResultSet getAllBroadByName(String partOfName) throws SQLException {
+        String query = "SELECT * FROM [dbo].[Broad] WHERE [Name] LIKE ?";
+        PreparedStatement pstmt = this.createPreparedStatement(query);
+        // modify the query here
+        pstmt.setString(1, "%" + partOfName + "%");
+        return executeQuery(pstmt);
+    }
+
+    public void toggleIsPublic(String broadID) throws SQLException {
+        String query = "UPDATE [dbo].[Broad] SET [isPublic] = NOT [isPublic] WHERE [BroadID] = ?";
+        PreparedStatement pstmt = this.createPreparedStatement(query);
+        pstmt.executeUpdate();
+    }
+
+    public ResultSet getAllBroadByID(String broadID) throws SQLException {
+        String query = "SELECT * FROM [dbo].[Broad] WHERE [BroadID] = ?";
+        PreparedStatement pstmt = this.createPreparedStatement(query);
+        // modify the query here
+        pstmt.setString(1, broadID);
+        return executeQuery(pstmt);
+    }
+
+    public ResultSet getLastestBroadID() throws SQLException {
+        String query = "SELECT [BroadID] FROM [dbo].[Broad] ORDER BY [BroadID] DESC LIMIT 1";
+        PreparedStatement pstmt = this.createPreparedStatement(query);
+        // modify the query here
+        return executeQuery(pstmt);
+    }
+
     /**
      * Create instance for PreparedStatement class
      *
