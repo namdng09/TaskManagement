fc9dab23ebdc7bd55a247aeeedefb86b1d777d1d
namdng09 <144111926+namdng09@users.noreply.github.com>
2024-06-27 08:32:03 +0700

    feat: dao file to get value in Comment table in database(#29)

--- TaskManager/src/java/dao/CommentDAO.java
+++ TaskManager/src/java/dao/CommentDAO.java
@@ -1,10 +1,46 @@
 package dao;
 
 import java.sql.*;
-import model.Comment;
 
 public class CommentDAO {
-    
+
+    public ResultSet getAllCommentByCardID(String cardID) throws SQLException {
+        String query = "SELECT [CommentID],[User].[FirstName],[User].[LastName],[CreatedDate],[Comment]"
+                + "FROM [dbo].[Comment]"
+                + "INNER JOIN [dbo].[User] ON [User].[User_UID] = [Comment].[User_UID]"
+                + "WHERE [CardID] = ?"
+                + "GROUP BY [CommentID],[User].[FirstName],[User].[LastName],[Comment]";
+        PreparedStatement pstmt = createPreparedStatement(query);
+        // modify the query here
+        pstmt.setString(1, cardID);
+        return executeQuery(pstmt);
+    }
+
+    public void deleteComment(String commentID) throws SQLException {
+        String query = "DELETE FROM [dbo].[Comment] WHERE [CommentID] = ?";
+        PreparedStatement pstmt = this.createPreparedStatement(query);
+        // modify the query here
+        pstmt.setString(1, commentID);
+
+        pstmt.executeUpdate();
+    }
+
+    public void updateComment(String commentID, String comment) throws SQLException {
+        String query = "UPDATE [dbo].[Comment] SET [Comment] = ? WHERE [CommentID] = ?";
+        PreparedStatement pstmt = this.createPreparedStatement(query);
+        // modify the query here
+        pstmt.setString(1, comment);
+        pstmt.setString(2, commentID);
+
+        pstmt.executeUpdate();
+    }
+
+    public ResultSet getLastestCommentID() throws SQLException {
+        String query = "SELECT [CommentID] FROM [dbo].[Comment] ORDER BY [CommentID] DESC LIMIT 1";
+        PreparedStatement pstmt = this.createPreparedStatement(query);
+        // modify the query here
+        return executeQuery(pstmt);
+    }
     /**
      * Create instance for PreparedStatement class
      *
