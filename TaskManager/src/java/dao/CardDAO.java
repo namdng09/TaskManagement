22c8562423f2d9380c95bfdd6030f0a903ff8110
namdng09 <144111926+namdng09@users.noreply.github.com>
2024-06-27 08:32:31 +0700

    feat: dao file to get value in Card table in database(#29)

--- TaskManager/src/java/dao/CardDAO.java
+++ TaskManager/src/java/dao/CardDAO.java
@@ -1,10 +1,39 @@
 package dao;
 
 import java.sql.*;
-import model.User;
+import model.Card;
 
 public class CardDAO {
-    
+
+    public void insertCard(Card card, String listTaskID) throws SQLException {
+        String query = "INSERT INTO [dbo].[Card]"
+                + "([CardID],[ListID],[Name],[Description],[CreatedDate])"
+                + "VALUES (?,?,?,?,?)";
+        PreparedStatement pstmt = createPreparedStatement(query);
+        pstmt.setString(1, card.getCardID());
+        pstmt.setString(2, listTaskID);
+        pstmt.setString(3, card.getCardName());
+        pstmt.setString(4, card.getDescription());
+        pstmt.setDate(5, card.getCreatedDate());
+
+        pstmt.executeUpdate();
+    }
+
+    public ResultSet getAllCardByListTaskID(String listTaskID) throws SQLException {
+        String query = "SELECT * FROM [dbo].[Card] WHERE [ListTaskID] = ?";
+        PreparedStatement pstmt = createPreparedStatement(query);
+        // modify the query here
+        pstmt.setString(1, listTaskID);
+        return executeQuery(pstmt);
+    }
+
+    public ResultSet getLastestCardID() throws SQLException {
+        String query = "SELECT [CardID] FROM [dbo].[Card] ORDER BY [CardID] DESC LIMIT 1";
+        PreparedStatement pstmt = createPreparedStatement(query);
+        // modify the query here
+        return executeQuery(pstmt);
+    }
+
     /**
      * Create instance for PreparedStatement class
      *
