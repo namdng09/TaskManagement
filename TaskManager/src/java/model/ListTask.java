6221f8b6244cf0b8b17313d72cab4077ceac5c73
namdng09 <144111926+namdng09@users.noreply.github.com>
2024-06-27 08:21:48 +0700

    feat: new model to store ListTask(#29)

--- TaskManager/src/java/model/ListTask.java
+++ TaskManager/src/java/model/ListTask.java
@@ -2,20 +2,25 @@ package model;
 
 import java.sql.*;
 import java.util.ArrayList;
+import dao.ListTaskDAO;
 
 public class ListTask {
+
     private String listTaskID;
     private String listTaskName;
-    private String position;
     private ArrayList<Card> cards;
 
     public ListTask() {
     }
 
-    public ListTask(String listTaskID, String listTaskName, String position, ArrayList<Card> cards) {
+    public ListTask(String listTaskID, String listTaskName) {
         this.listTaskID = listTaskID;
         this.listTaskName = listTaskName;
-        this.position = position;
+    }
+
+    public ListTask(String listTaskID, String listTaskName, ArrayList<Card> cards) {
+        this.listTaskID = listTaskID;
+        this.listTaskName = listTaskName;
         this.cards = cards;
     }
 
@@ -35,14 +40,6 @@ public class ListTask {
         this.listTaskName = listTaskName;
     }
 
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
     public ArrayList<Card> getCards() {
         return cards;
     }
@@ -53,8 +50,61 @@ public class ListTask {
 
     @Override
     public String toString() {
-        return "ListTask{" + "listTaskID=" + listTaskID + ", listTaskName=" + listTaskName + ", position=" + position + ", cards=" + cards + '}';
+        return "ListTask{" + "listTaskID=" + listTaskID + ", listTaskName=" + listTaskName + ", cards=" + cards + '}';
     }
-    
-    
+
+    public ArrayList<ListTask> getAllListTaskByBroadID(String broadID) {
+        ListTaskDAO listTaskDAO = new ListTaskDAO();
+        ArrayList<ListTask> listTask = new ArrayList<>();
+        try {
+            ResultSet rs = listTaskDAO.getAllListTaskByBroadID(broadID);
+            while (rs.next()) {
+                String id = rs.getString("ListTaskID");
+                String name = rs.getString("Name");
+                ArrayList<Card> cards = (new Card()).getAllCardByListTaskID(id);
+
+                ListTask task = new ListTask(id, name, cards);
+                listTask.add(task);
+            }
+        } catch (SQLException e) {
+            //TODO: handle exception
+            System.out.println("ERROR: " + e.getMessage());
+        }
+        return listTask;
+    }
+
+    public void createListTask(String listTaskID, String broadID, String name) {
+        ListTaskDAO listTaskDAO = new ListTaskDAO();
+        try {
+            ListTask listTask = new ListTask(listTaskID, name);
+            listTaskDAO.insertListTask(listTask, broadID);
+        } catch (SQLException e) {
+            //TODO: handle exception
+            System.out.println("ERROR: " + e.getMessage());
+        }
+    }
+
+    public String generateListTaskID() {
+        ListTaskDAO listTaskDAO = new ListTaskDAO();
+        String lastID = null;
+        try {
+            ResultSet rs = listTaskDAO.getLastestListTaskID();
+            if (rs.next()) {
+                lastID = rs.getString("listTaskID");
+            }
+
+            if (lastID == null) {
+                lastID = "LT0001";
+            } else {
+                int idNum = Integer.parseInt(lastID.substring(2));
+                idNum++;
+                lastID = String.format("LT%04d", idNum);
+            }
+        } catch (SQLException e) {
+            //TODO: handle exception
+            System.out.println("ERROR: " + e.getMessage());
+        }
+        return lastID;
+    }
+
 }
