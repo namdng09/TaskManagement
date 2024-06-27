4b44c3428ccc4afc0deedc10a6bd3e102f459d61
namdng09 <144111926+namdng09@users.noreply.github.com>
2024-06-27 08:17:24 +0700

    feat: add and move func to check logic into model Ãá (#29)

--- TaskManager/src/java/model/User.java
+++ TaskManager/src/java/model/User.java
@@ -10,12 +10,12 @@ import java.sql.*;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author namdng09
  */
-public class User{
+public class User {
+
     private String userUID;
     private String username;
     private String email;
@@ -117,19 +117,19 @@ public class User{
      */
     public void checkValidUserName(String username) throws Exception {
         Validation validate = new Validation();
-        UserDAO dao = new UserDAO();
 
         String message;
         if (!validate.isValidSyntaxUsername(username)) {
             message = "Username is not valid!";
             throw new Exception(message);
         }
-        if (dao.isExistUserName(username)) {
+        if (this.isExistUserName(username)) {
             message = "Username is already existed!";
             throw new Exception(message);
         }
     }
-    public void checkValidPhoneNumber(String phone) throws Exception{
+
+    public void checkValidPhoneNumber(String phone) throws Exception {
         Validation validate = new Validation();
         String message;
 
@@ -148,14 +148,13 @@ public class User{
      */
     public void checkValidEmail(String email) throws Exception {
         Validation validate = new Validation();
-        UserDAO dao = new UserDAO();
 
         String message;
         if (!validate.isValidSyntaxEmail(email)) {
             message = "Email is not valid!";
             throw new Exception(message);
         }
-        if (dao.isExistEmail(email)) {
+        if (this.isExistEmail(email)) {
             message = "Email is already existed!";
             throw new Exception(message);
         }
@@ -177,27 +176,52 @@ public class User{
     }
 
     /**
+     * Validates a name to ensure it meets the required syntax.
+     *
+     * @param name
+     * @throws Exception if the password syntax is invalid.
+     */
+    public void checkValidNameString(String name) throws Exception {
+        Validation validate = new Validation();
+        String message;
+        if (!validate.isValidSyntaxNameString(name)) {
+            message = "Name can not have special word!";
+            throw new Exception(message);
+        }
+    }
+    /**
      * Registers a new user by validating the inputs and storing the user
      * information.
      *
      * @param username The username for the new user.
      * @param email The email address for the new user.
      * @param password The password for the new user.
+     * @param firstName
+     * @param lastName
+     * @param birthDate
+     * @param phoneNumber
      */
     public void registerUser(String username, String email, String password, String firstName, String lastName, Date birthDate, String phoneNumber) {
         Utility utils = new Utility();
-        UserDAO dao = new UserDAO();
+        UserDAO userDAO = new UserDAO();
 
         String user_uid = utils.generateUserUID();
+        String fName = utils.formatNameString(firstName);
+        String lName = utils.formatNameString(lastName);
 
         // Using BCrypt to make hashing password convenient
         String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt(13));
 
-        User user = new User(user_uid, username, email, hashedPassword, firstName, lastName, birthDate, phoneNumber);
+        try {
+            User user = new User(user_uid, username, email, hashedPassword, fName, lName, birthDate, phoneNumber);
 
-        dao.insertUser(user);
+            userDAO.insertUser(user);
+        } catch (SQLException e) {
+            //TODO: handle exception
+            System.out.println("ERROR: " + e.getMessage());
+        }
     }
-    
+
     public boolean checkValidLogin(String username, String password) {
         UserDAO dao = new UserDAO();
         boolean flag = false;
@@ -211,14 +235,14 @@ public class User{
             }
         } catch (SQLException e) {
             //TODO: handle exception
-            System.out.println("ERROR: " + e.getMessage());
-        }
-        return flag;
+                System.out.println("ERROR: " + e.getMessage());
+            }
+            return flag;
     }
 
     public User getAccountByUsername(String username) {
         UserDAO dao = new UserDAO();
-        User acc = null;
+        User account = null;
         try {
             ResultSet rs = dao.getUserByUsername(username);
             if (rs.next()) {
@@ -226,16 +250,59 @@ public class User{
                 String userName = rs.getString("Username");
                 String userEmail = rs.getString("Email");
                 String userPassword = rs.getString("Password");
-                String userFirstName = rs.getString("FirstName");
-                String userLastName  = rs.getString("LastName");
+                String fName = rs.getString("FirstName");
+                String lName = rs.getString("LastName");
                 Date userBirthDate = rs.getDate("BirthDate");
                 String userPhoneNumber = rs.getString("Phone");
-                acc = new User(uid, userName, userEmail, userPassword, userFirstName, userLastName, userBirthDate, userPhoneNumber);
+                account = new User(uid, userName, userEmail, userPassword, fName, lName, userBirthDate, userPhoneNumber);
             }
-        } catch (Exception e) {
+        } catch (SQLException e) {
             //TODO: handle exception
             System.out.println("ERROR" + e.getMessage());
         }
-        return acc;
+        return account;
     }
+
+    /**
+     * Checks if a username exist in the database
+     *
+     * @param username The username to check for existence
+     * @return true if the username exist, false otherwise.j
+     */
+    public boolean isExistUserName(String username) {
+        UserDAO userDAO = new UserDAO();
+        boolean flag = false;
+        try {
+            ResultSet rs = userDAO.getUserByUsername(username);
+            if (rs.next()) {  // Mean have duplicate username
+                flag = true;
+            }
+        } catch (SQLException e) {
+            //TODO: handle exception
+            System.out.println("ERROR: " + e.getMessage());
+        }
+        return flag;
+    }
+
+    /**
+     * Checks if an email exists in the database.
+     *
+     * @param email The email address to check for existence.
+     * @return true if the email exists, false otherwise.
+     */
+    public boolean isExistEmail(String email) {
+        UserDAO userDAO = new UserDAO();
+        boolean flag = false;
+        try {
+            ResultSet rs = userDAO.getUserByEmail(email);
+            if (rs.next()) {  // Mean have duplicate email
+                flag = true;
+            }
+        } catch (SQLException e) {
+            //TODO: handle exception
+            System.out.println("ERROR: " + e.getMessage());
+        }
+        return flag;
+    }
+
 }
