0806685a2623a6b2db894b58f6c5e9ec614078c7
namdng09 <144111926+namdng09@users.noreply.github.com>
2024-07-01 15:19:30 +0700

    fix: set Cookie in doGet func (#36)

--- TaskManager/src/java/controller/LoginServlet.java
+++ TaskManager/src/java/controller/LoginServlet.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import java.io.IOException;
@@ -21,34 +20,37 @@ import model.User;
  * @author namdng09
  */
 public class LoginServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");  
+            out.println("<title>Servlet LoginServlet</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -56,12 +58,27 @@ public class LoginServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
+        Cookie arr[] = request.getCookies();
+        if (arr != null) {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i].getName().equals("cName")) {
+                    request.setAttribute("uName", arr[i].getValue());
+                }
+                if (arr[i].getName().equals("cPass")) {
+                    request.setAttribute("uPass", arr[i].getValue());
+                }
+                if (arr[i].getName().equals("cRem")) {
+                    request.setAttribute("reMem", arr[i].getValue());
+                }
+            }
+        }
         request.getRequestDispatcher("login.jsp").forward(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -69,7 +86,7 @@ public class LoginServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         User user = new User();
 
         String username = request.getParameter("name");
@@ -78,9 +95,9 @@ public class LoginServlet extends HttpServlet {
         String status;
         String message;
 
-        Cookie userCookie = new Cookie("cname", username);
-        Cookie passCookie = new Cookie("cpass", password);
-        Cookie remCookie = new Cookie("crem", password);
+        Cookie userCookie = new Cookie("cName", username);
+        Cookie passCookie = new Cookie("cPass", password);
+        Cookie remCookie = new Cookie("cRem", password);
 
         if (rem != null) {
             userCookie.setMaxAge(60 * 60 * 24);
@@ -105,12 +122,13 @@ public class LoginServlet extends HttpServlet {
             user = user.getAccountByUsername(username);
             HttpSession section = request.getSession();
             section.setAttribute("account", user);
-            response.sendRedirect("index.html");
+            response.sendRedirect("home");
         }
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
